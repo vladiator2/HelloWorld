0af2f252e32db73bb46691e4b80f6c8ddda19bbf
Vlad Antipov <vlad_antipov@hotmail.com>
2020-01-15 15:52:18 -0600

    First Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Vlad Antipov on 1/15/20.
+//  Copyright © 2020 Vlad Antipov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
